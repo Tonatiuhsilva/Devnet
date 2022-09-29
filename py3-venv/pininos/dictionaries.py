6c82859943899c4fd58600f82b78adc48276e5da
asilva2 <asilva2@cisco.com>
2022-09-28 23:27:31 -0500

    add all pininos files

---
+++ py3-venv/pininos/dictionaries.py
@@ -0,0 +1,20 @@
+# Fill in this file with the code for Python practice with data structures
+
+def legalAges():
+    lifeEvent = {
+          "drivingAge": 16,
+          "votingAge": 18,
+          "drinkingAge": 21,
+          "retirementAge": 65
+      }
+    
+    print("The legal driving age is "
+      + str(lifeEvent["drivingAge"]) + ".")
+    print("The legal voting age is "
+      + str(lifeEvent["votingAge"]) + ".")
+    print("The legal drinking age is "
+      + str(lifeEvent["drinkingAge"]) + ".")
+    print("The legal retirement age is "
+      + str(lifeEvent["retirementAge"]) + ".")
+    
+legalAges()
